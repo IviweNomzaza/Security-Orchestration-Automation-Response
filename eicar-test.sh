a1600862fc4cfd3e02da1ff9a51f7d292f100b4b
ThunderHater421 <iviwenomzaza51@gmail.com>
2026-09-11 21:41:03 +0200

    Cleanup comments in eicar-test.sh
    Removed comments explaining the purpose of the script.

--- eicar-test.sh
+++ eicar-test.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Drops the standard EICAR test string on the monitored endpoint to
-# trigger the Wazuh -> Shuffle -> TheHive pipeline end to end.
 cd /home/mintuser/eicar_test && wget https://secure.eicar.org/eicar.com.txt -O eicar_critical.txt
